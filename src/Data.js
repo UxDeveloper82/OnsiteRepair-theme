90bab92f3c09476fddb0905488bb711c11398201
Martin Fernandez <Electronicsmf@aol.com>
2023-02-15 20:24:32 -0500

    Adding Shopping area

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Heating Element",
+      price: 145,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "content/images/repairs/washer-dryer.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Dishwasher Pump",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "content/images/repairs/dishwasher.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Bake Element",
+      price: 125,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "content/images/repairs/cooktop.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Compressor",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "content/images/repairs/refrigator.jpg",
+    },
+  ];
